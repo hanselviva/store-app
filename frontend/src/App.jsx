8aac57ff377c36ce3256c1b1a3a28af7cb28c444
HanselV <hanselviva@gmail.com>
2021-06-25 22:04:46 -0700

    added components inside App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 //
-import Header from "./components/Header/Header"
-
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Homepage from "./components/Homepage/Homepage";
+import Login from "./components/Login/Login";
+import Store from "./components/Store/Store";
 
 function App() {
-  return (
-    <div className="App">
-
-    <Header />
-      <div className="app-content">
-
-      </div>
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<div className="app-content">
+				<Switch>
+					<Route exact path="/" component={Homepage} />
+					<Route path="/login" component={Login} />
+					<Route path="/store" component={Store} />
+				</Switch>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
