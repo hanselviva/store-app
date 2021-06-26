5c6f1ec21a1fbc465c4c379caa041c982e1390f1
HanselV <hanselviva@gmail.com>
2021-06-26 12:10:01 -0700

    added image in homepage

--- frontend/src/components/Homepage/Homepage.js
+++ frontend/src/components/Homepage/Homepage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { CssBaseline, Paper, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import playingGames from "../../assets/playinggames.png";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		height: "100vh",
 	},
 	image: {
-		backgroundImage: "url(https://source.unsplash.com/random)",
+		backgroundImage: `url(${playingGames})`,
 		backgroundRepeat: "no-repeat",
 		backgroundColor:
 			theme.palette.type === "light"
@@ -24,15 +25,7 @@ const Homepage = () => {
 		<div className="homepageWrapper">
 			<Grid container component="main" className={classes.root}>
 				<CssBaseline />
-				<Grid
-					item
-					xs={12}
-					sm={8}
-					md={5}
-					component={Paper}
-					elevation={6}
-					square
-				></Grid>
+				<Grid item xs={12} sm={8} md={5} elevation={6} square></Grid>
 				<Grid item xs={false} sm={4} md={7} className={classes.image} />
 			</Grid>
 		</div>
